f24c55cee8eda2ceb70612a80ca6511e9f2b6695
222212602 <222212602@stis.ac.id>
2024-08-20 18:24:25 +0700

    penambahan file text.php

---
+++ resources/views/test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Hello world
+    </h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus voluptates adipisci laboriosam possimus
+        exercitationem, aliquam officia esse placeat asperiores maiores enim sapiente quaerat harum commodi ipsa quos
+        ipsum delectus reprehenderit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium saepe nam
+        velit vitae ab atque nobis repudiandae perferendis aperiam earum, vero est explicabo temporibus, quidem dolor
+        eaque culpa blanditiis corrupti?</p>
+</body>
+
+</html>
