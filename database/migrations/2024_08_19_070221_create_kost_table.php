abca34b5e6d4dc7e9248ec3ed94caa131b8a0291
Antonius Bagas Sunu Wiguna Ardy <222212509@stis.ac.id>
2024-08-23 12:01:38 +0700

    detail-k update

--- database/migrations/2024_08_19_070221_create_kost_table.php
+++ database/migrations/2024_08_19_070221_create_kost_table.php
@@ -12,31 +12,37 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('kosts', function (Blueprint $table) {
-            $table->id();
+            $table->id(); // Auto-incrementing primary key
             $table->string('nama_kos', 100);
             $table->string('slug', 100);
             $table->string('pemilik', 100);
+            $table->string('telp_pemilik', 50);
             $table->string('lokasi', 100);
-            $table->string('luas_kamar', 100);
-            $table->integer('harga');
+            $table->integer('panjang');
+            $table->integer('lebar');
+            $table->integer('kamar_kosong');
+            $table->integer('harga_min');
+            $table->integer('harga_max');
             $table->string('batasan_agama', 200);
-            $table->string('s_pendingin', 50);
-            $table->string('fasilitas', 100);
+            $table->string('s_ac', 30)->default('Tidak tersedia');
+            $table->string('s_kipasangin', 30)->default('Tidak tersedia');
             $table->text('deskripsi');
-            $table->boolean('s_wifi');
-            $table->string('s_kamar_mandi', 50);
-            $table->string('pinpoint', 50);
+            $table->string('s_wifi', 20)->default('Tidak tersedia');
+            $table->string('s_kamar_mandi', 50)->default('Tidak tersedia');
+            $table->string('pinpoint', 200);
             $table->string('batasan_mahasiswa', 50);
             $table->string('tipe_kos', 50);
             $table->string('tipe_bayar', 50);
             $table->integer('watt_listrik');
-            $table->string('s_listrik', 50);
-            $table->string('s_mejabelajar', 50);
-            $table->string('s_kasur', 50);
-            $table->string('foto', 50);
-            $table->string('s_dapur', 50);
-
-            $table->timestamps();
+            $table->string('s_listrik', 50)->default('Tidak tersedia');
+            $table->string('s_mejabelajar', 50)->default('Tidak tersedia');
+            $table->string('s_kasur', 50)->default('Tidak tersedia');
+            $table->string('s_parkir', 30)->default('Tidak tersedia');
+            $table->string('s_lemari', 50)->default('Tidak tersedia');
+            $table->string('s_mesincuci', 20)->default('Tidak tersedia');
+            $table->string('foto', 200);
+            $table->string('s_dapur', 50)->default('Tidak tersedia');
+            $table->timestamps(); // created_at and updated_at
         });
     }
 
--- resources/views/detail-k.blade.php
+++ resources/views/detail-k.blade.php
@@ -46,141 +46,34 @@
                 <!-- Options -->
                 <div class="mt-4 lg:row-span-3 lg:mt-0">
                     <h2 class="sr-only">Product information</h2>
-                    <p class="text-3xl font-bold tracking-tight text-gray-900">Rp999.000</p>
+                    <p class="text-3xl font-bold tracking-tight text-gray-900">Rp999.000-1.199.000</p>
 
                     <!-- Reviews -->
 
 
                     <form class="mt-10">
                         <!-- Colors -->
-                        <div>
-                            <h3 class="text-sm font-medium text-gray-900">Fasilitas</h3>
-                            <x-wifi-svg></x-wifi-svg>
-                            <x-toilet-svg></x-toilet-svg>
-                            <x-parkir-svg></x-parkir-svg>
-                        </div>
 
                         <!-- Sizes -->
                         <div class="mt-10">
-                            <div class="flex items-center justify-between">
-                                <h3 class="text-sm font-medium text-gray-900">Size</h3>
-                                <a href="#" class="text-sm font-medium text-indigo-600 hover:text-indigo-500">Size
-                                    guide</a>
+                            <div class="justify-between">
+                                <h3 class="text-sm font-medium text-gray-900">Pemilik</h3>
+                                <a href="https://google.com" class="flex justify-between mt-4 border border-gray-400 px-3 py-3 rounded-xl">
+                                    <div>
+                                        <p class="text-2xl font-medium">Bu Ida</p>
+                                        <p class="">+62123456789</p>
+                                    </div>
+                                    <img src="https://seeklogo.com/images/W/whatsapp-logo-0DBD89C8E2-seeklogo.com.png" class="h-10" alt="">
+                                </a>
                             </div>
-
-                            <fieldset aria-label="Choose a size" class="mt-4">
-                                <div class="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
-                                    <!-- Active: "ring-2 ring-indigo-500" -->
-                                    <label
-                                        class="group relative flex cursor-not-allowed items-center justify-center rounded-md border bg-gray-50 px-4 py-3 text-sm font-medium uppercase text-gray-200 hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6">
-                                        <input type="radio" name="size-choice" value="XXS" disabled
-                                            class="sr-only">
-                                        <span>XXS</span>
-                                        <span aria-hidden="true"
-                                            class="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200">
-                                            <svg class="absolute inset-0 h-full w-full stroke-2 text-gray-200"
-                                                viewBox="0 0 100 100" preserveAspectRatio="none" stroke="currentColor">
-                                                <line x1="0" y1="100" x2="100" y2="0"
-                                                    vector-effect="non-scaling-stroke" />
-                                            </svg>
-                                        </span>
-                                    </label>
-                                    <!-- Active: "ring-2 ring-indigo-500" -->
-                                    <label
-                                        class="group relative flex cursor-pointer items-center justify-center rounded-md border bg-white px-4 py-3 text-sm font-medium uppercase text-gray-900 shadow-sm hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6">
-                                        <input type="radio" name="size-choice" value="XS" class="sr-only">
-                                        <span>XS</span>
-                                        <!--
-                          Active: "border", Not Active: "border-2"
-                          Checked: "border-indigo-500", Not Checked: "border-transparent"
-                        -->
-                                        <span class="pointer-events-none absolute -inset-px rounded-md"
-                                            aria-hidden="true"></span>
-                                    </label>
-                                    <!-- Active: "ring-2 ring-indigo-500" -->
-                                    <label
-                                        class="group relative flex cursor-pointer items-center justify-center rounded-md border bg-white px-4 py-3 text-sm font-medium uppercase text-gray-900 shadow-sm hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6">
-                                        <input type="radio" name="size-choice" value="S" class="sr-only">
-                                        <span>S</span>
-                                        <!--
-                          Active: "border", Not Active: "border-2"
-                          Checked: "border-indigo-500", Not Checked: "border-transparent"
-                        -->
-                                        <span class="pointer-events-none absolute -inset-px rounded-md"
-                                            aria-hidden="true"></span>
-                                    </label>
-                                    <!-- Active: "ring-2 ring-indigo-500" -->
-                                    <label
-                                        class="group relative flex cursor-pointer items-center justify-center rounded-md border bg-white px-4 py-3 text-sm font-medium uppercase text-gray-900 shadow-sm hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6">
-                                        <input type="radio" name="size-choice" value="M" class="sr-only">
-                                        <span>M</span>
-                                        <!--
-                          Active: "border", Not Active: "border-2"
-                          Checked: "border-indigo-500", Not Checked: "border-transparent"
-                        -->
-                                        <span class="pointer-events-none absolute -inset-px rounded-md"
-                                            aria-hidden="true"></span>
-                                    </label>
-                                    <!-- Active: "ring-2 ring-indigo-500" -->
-                                    <label
-                                        class="group relative flex cursor-pointer items-center justify-center rounded-md border bg-white px-4 py-3 text-sm font-medium uppercase text-gray-900 shadow-sm hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6">
-                                        <input type="radio" name="size-choice" value="L" class="sr-only">
-                                        <span>L</span>
-                                        <!--
-                          Active: "border", Not Active: "border-2"
-                          Checked: "border-indigo-500", Not Checked: "border-transparent"
-                        -->
-                                        <span class="pointer-events-none absolute -inset-px rounded-md"
-                                            aria-hidden="true"></span>
-                                    </label>
-                                    <!-- Active: "ring-2 ring-indigo-500" -->
-                                    <label
-                                        class="group relative flex cursor-pointer items-center justify-center rounded-md border bg-white px-4 py-3 text-sm font-medium uppercase text-gray-900 shadow-sm hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6">
-                                        <input type="radio" name="size-choice" value="XL" class="sr-only">
-                                        <span>XL</span>
-                                        <!--
-                          Active: "border", Not Active: "border-2"
-                          Checked: "border-indigo-500", Not Checked: "border-transparent"
-                        -->
-                                        <span class="pointer-events-none absolute -inset-px rounded-md"
-                                            aria-hidden="true"></span>
-                                    </label>
-                                    <!-- Active: "ring-2 ring-indigo-500" -->
-                                    <label
-                                        class="group relative flex cursor-pointer items-center justify-center rounded-md border bg-white px-4 py-3 text-sm font-medium uppercase text-gray-900 shadow-sm hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6">
-                                        <input type="radio" name="size-choice" value="2XL" class="sr-only">
-                                        <span>2XL</span>
-                                        <!--
-                          Active: "border", Not Active: "border-2"
-                          Checked: "border-indigo-500", Not Checked: "border-transparent"
-                        -->
-                                        <span class="pointer-events-none absolute -inset-px rounded-md"
-                                            aria-hidden="true"></span>
-                                    </label>
-                                    <!-- Active: "ring-2 ring-indigo-500" -->
-                                    <label
-                                        class="group relative flex cursor-pointer items-center justify-center rounded-md border bg-white px-4 py-3 text-sm font-medium uppercase text-gray-900 shadow-sm hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6">
-                                        <input type="radio" name="size-choice" value="3XL" class="sr-only">
-                                        <span>3XL</span>
-                                        <!--
-                          Active: "border", Not Active: "border-2"
-                          Checked: "border-indigo-500", Not Checked: "border-transparent"
-                        -->
-                                        <span class="pointer-events-none absolute -inset-px rounded-md"
-                                            aria-hidden="true"></span>
-                                    </label>
-                                </div>
-                            </fieldset>
                         </div>
 
                         <button type="submit"
-                            class="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Hubungi
-                            Pemilik</button>
+                            class="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Buka di Maps</button>
                     </form>
                 </div>
 
-                <div
-                    class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
+                <div class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
                     <!-- Description and details -->
                     <div>
                         <h3 class="sr-only">Deskripsi</h3>
@@ -190,6 +83,12 @@
                                 vibrant personality with three grayscale options. Feeling adventurous? Put on a heather
                                 gray tee. Want to be a trendsetter? Try our exclusive colorway: &quot;Black&quot;. Need
                                 to add an extra pop of color to your outfit? Our white tee has you covered.</p>
+                            <div>
+                                <h3 class="text-sm font-medium text-gray-900">Fasilitas</h3>
+                                <x-wifi-svg></x-wifi-svg>
+                                <x-toilet-svg></x-toilet-svg>
+                                <x-parkir-svg></x-parkir-svg>
+                            </div>
                         </div>
                     </div>
 
